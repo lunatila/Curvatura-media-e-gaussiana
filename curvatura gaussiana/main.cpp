c2f6f45851b3eefc6ea9ed28af1808bda470d24d
lunatila <lacucaratila@gmail.com>
2022-07-05 15:33:53 -0300

    Initial Commit

---
+++ curvatura gaussiana/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  curvatura gaussiana
+//
+//  Created by Átila Luna Ambrósio da Silva on 05/07/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
